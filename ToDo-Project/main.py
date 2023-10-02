6ae09b6b137304390322ff75064a5d90f258de8d
Amin Zayeromali <amin.zayeromali@gmail.com>
2023-10-02 19:12:07 +0330

    initiate codes ...

--- ToDo-Project/main.py
+++ ToDo-Project/main.py
@@ -1,31 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 
 app = FastAPI()
 
-todos = []
 
-
-@app.post("/todos/")
-def create_todo(todo: str):
-    todos.append(todo)
-    return {"message": "Todo created successfully"}
-
-
-@app.get("/todos/")
-def get_todos():
-    return todos
-
-
-@app.get("/todos/{todo_id}")
-def get_todo(todo_id: int):
-    if todo_id < 0 or todo_id >= len(todos):
-        raise HTTPException(status_code=404, detail="Todo not found")
-    return todos[todo_id]
-
-
-@app.delete("/todos/{todo_id}")
-def delete_todo(todo_id: int):
-    if todo_id < 0 or todo_id >= len(todos):
-        raise HTTPException(status_code=404, detail="Todo not found")
-    deleted_todo = todos.pop(todo_id)
-    return {"message": f"Todo '{deleted_todo}' deleted successfully"}
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the To-Do List API"}
